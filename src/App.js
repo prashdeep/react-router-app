1cf9c397bf3596c47351a34198037d0e6e37ae71
pradeep <pradeep.kumar44@gmail.com>
2019-06-21 07:41:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,  NavLink, Redirect} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state={
+    loggedIn: false
+  }
+  handleLogin = ()=> {
+    this.setState(previousState => ({
+      loggedIn: !previousState.loggedIn
+    }))
+  }
+  render(){
+    return (
+      <BrowserRouter>
+      <div className="App">
+        <ul>
+          <li> 
+            <NavLink activeStyle={{color:'red'}} exact to="/">Home</NavLink>
+          </li>  
+          <li>
+            <NavLink activeStyle={{color:'red'}} exact to="/about">About</NavLink>
+            </li> 
+        </ul>
+        <input type= "button" value={this.state.loggedIn ? 'logout':'log in'} onClick={this.handleLogin.bind(this)}/>
+      <Route path="/" exact strict render={
+        ()=>{
+          return (<h1>Home page</h1>)
+        }
+      }/>
+      
+      <Route path="/about" exact strict render={
+        ()=>{
+          return (<h1>About page</h1>)
+        }
+      }/>
+      
+
+      <Route path="/user/:username" exact strict render={({ match})=>(
+        this.state.loggedIn ? (<User username={match.params.username}/>):(<Redirect to='/'/>)
+      )}/>
+      </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+const User = (params) =>{
+  return (<h1>Welcome User {params.username}</h1>)
 }
 
 export default App;
